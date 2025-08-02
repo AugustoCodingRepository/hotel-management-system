3f5c6adadd703b8d9c4d22495c63dfa01a3aef8b
augustopersico <a.persico14@studenti.unisa.it>
2025-08-02 16:28:19 +0200

    Resolve merge conflicts

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,16 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ['mongodb']
   },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+      crypto: false,
+    }
+    return config
+  },
   env: {
     MONGODB_URI: process.env.MONGODB_URI,
   },
